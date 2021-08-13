088263faa8b8393d57e8e512fadcd57dc03dff5c
dimitree54 <dimitree54@gmail.com>
2021-08-13 21:16:39 +0300

    release 0.0.2-alpha

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ sourceSets.test {
 	java.srcDirs("src/test")
 }
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+	kotlinOptions.jvmTarget = "1.8"
+}
+
 publishing {
 	publications {
 		create<MavenPublication>("default") {
