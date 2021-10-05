30c2ed81e368f59a7daa440e5d36ec35368cd472
Dmitrii Rashchenko <dimitree54@gmail.com>
2021-10-05 21:52:41 +0300

    tests for time controller

---
+++ src/test/we/rashchenko/networks/controllers/TimeControllerTest.kt
@@ -0,0 +1,40 @@
+package we.rashchenko.networks.controllers
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import we.rashchenko.base.Feedback
+import we.rashchenko.neurons.ControlledNeuron
+import we.rashchenko.neurons.SlowNeuron
+import we.rashchenko.neurons.zoo.RandomNeuron
+
+internal class TimeControllerTest {
+
+    @Test
+    fun getControllerFeedbacks() {
+        val neurons = listOf(
+            ControlledNeuron(SlowNeuron()).apply{control=true},
+            ControlledNeuron(RandomNeuron(0.5f)).apply{control=true},
+            ControlledNeuron(RandomNeuron(0.5f)).apply{control=true},
+            ControlledNeuron(RandomNeuron(0.5f)).apply{control=true},
+            ControlledNeuron(SlowNeuron()).apply{control=true},
+        )
+
+        repeat(1000){ timeStep->
+            neurons.forEach{
+                it.touch(timeStep, timeStep.toLong())
+                it.update(Feedback.NEUTRAL, timeStep.toLong())
+                it.getFeedback(timeStep)
+                it.forgetSource(timeStep)
+                it.active
+            }
+        }
+
+        val controllerFeedbacks = TimeController().getControllerFeedbacks(neurons)
+        assertTrue(controllerFeedbacks[0] < Feedback.NEUTRAL)
+        assertTrue(controllerFeedbacks[1] > Feedback.NEUTRAL)
+        assertTrue(controllerFeedbacks[2] > Feedback.NEUTRAL)
+        assertTrue(controllerFeedbacks[3] > Feedback.NEUTRAL)
+        assertTrue(controllerFeedbacks[4] < Feedback.NEUTRAL)
+    }
+}
