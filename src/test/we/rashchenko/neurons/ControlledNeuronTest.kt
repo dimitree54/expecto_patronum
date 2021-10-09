f6540d40f7f5513292bfa58297132929d2be908f
Dmitrii Rashchenko <dimitree54@gmail.com>
2021-10-09 18:16:15 +0300

    more stable (but slower) test parameters

--- src/test/we/rashchenko/neurons/ControlledNeuronTest.kt
+++ src/test/we/rashchenko/neurons/ControlledNeuronTest.kt
@@ -58,18 +58,18 @@ internal class ControlledNeuronTest {
         assertEquals(controlledNeuron.averageTouchTime, 0.0)
         assertEquals(controlledNeuron.averageForgetTime, 0.0)
 
-        repeat(100){
+        repeat(1000){
             controlledNeuron.touch(it, it.toLong())
             controlledNeuron.update(Feedback.NEUTRAL, it.toLong())
             controlledNeuron.getFeedback(it)
             controlledNeuron.forgetSource(it)
             controlledNeuron.active
         }
-        assertEquals(controlledNeuron.numControlledGetActive, 200)  // note that ControlledNeuron.update also uses getActive
-        assertEquals(controlledNeuron.numControlledForget, 100)
-        assertEquals(controlledNeuron.numControlledUpdate, 100)
-        assertEquals(controlledNeuron.numControlledTouch, 100)
-        assertEquals(controlledNeuron.numControlledGetFeedback, 100)
+        assertEquals(controlledNeuron.numControlledGetActive, 2000)  // note that ControlledNeuron.update also uses getActive
+        assertEquals(controlledNeuron.numControlledForget, 1000)
+        assertEquals(controlledNeuron.numControlledUpdate, 1000)
+        assertEquals(controlledNeuron.numControlledTouch, 1000)
+        assertEquals(controlledNeuron.numControlledGetFeedback, 1000)
         assertTrue(controlledNeuron.averageActivity in 0.4..0.6)
         assertTrue(controlledNeuron.averageGetActiveTime in 1.0..2.0)
         assertTrue(controlledNeuron.averageGetFeedbackTime in 1.0..2.0)
