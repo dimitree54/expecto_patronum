75017101519db605f1e98e4a66319ceefc307b70
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-07-15 20:02:46 +0300

    establish_contact.kt interface

--- src/main/we/rashchenko/patronum/telegram/establish_contact.kt
+++ src/main/we/rashchenko/patronum/telegram/establish_contact.kt
@@ -1,2 +1,10 @@
 package we.rashchenko.patronum.telegram
 
+import com.github.kotlintelegrambot.Bot
+import com.github.kotlintelegrambot.entities.ChatId
+import we.rashchenko.patronum.Wish
+
+fun establishContact(bot: Bot, chatId: ChatId.Id, telegramPatronId: Long, wish: Wish) {
+    // todo create chat with bot as moderator (not supported by Bot API, but can be done by command line)
+    bot.sendMessage(chatId, text="")
+}
