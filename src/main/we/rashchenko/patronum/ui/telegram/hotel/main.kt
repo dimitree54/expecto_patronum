78ab3243653fcf1c6515c944748d6cd85fcb1533
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-07-23 11:26:23 +0300

    remove debug code

--- src/main/we/rashchenko/patronum/ui/telegram/hotel/main.kt
+++
@@ -1,8 +0,0 @@
-package we.rashchenko.patronum.ui.telegram.hotel
-
-
-fun main() {
-    val moderatorUserId = 5326457106L
-    val hotel = TelegramHotel(moderatorUserId)
-    hotel.openRoom("test", listOf(530993342L))
-}
