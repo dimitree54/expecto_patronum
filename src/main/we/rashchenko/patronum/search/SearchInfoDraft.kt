c659d0b430809e52a8a1f0ac37868ad2b51e3fe1
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-08-03 10:20:46 +0300

    location jiggle

--- src/main/we/rashchenko/patronum/search/SearchInfoDraft.kt
+++ src/main/we/rashchenko/patronum/search/SearchInfoDraft.kt
@@ -4,12 +4,13 @@ import we.rashchenko.patronum.search.geo.Circle
 import we.rashchenko.patronum.search.geo.Location
 
 class SearchInfoDraft {
+    private val jiggleRate = 0.1f
     var location: Location? = null
     var radius: Float? = null
     fun toSearchInfo(): SearchInfo {
         val searchArea = location?.let { l ->
             radius?.let{r ->
-                Circle(l, r).toGeoPolygon()
+                Circle(l, r).toGeoPolygon(jiggleRate = jiggleRate)
             }
         }
         return SearchInfo(searchArea)
--- src/main/we/rashchenko/patronum/search/geo/Circle.kt
+++ src/main/we/rashchenko/patronum/search/geo/Circle.kt
@@ -1,23 +1,37 @@
 package we.rashchenko.patronum.search.geo
 
+import java.util.Random
 import kotlin.math.cos
 import kotlin.math.sin
 
 class Circle(private val center: Location, private val radiusInMeters: Float) {
     private val earthRadiusInMeters = 6371000f
-    fun toGeoPolygon(pointsPerCircle: Int = 16): Polygon{
+    private fun addXYToLocation(location: Location, dx: Float, dy: Float): Location {
+        val latitude  = location.latitude  + (dy / earthRadiusInMeters) * (180f / Math.PI.toFloat())
+        val longitude = location.longitude + (dx / earthRadiusInMeters) * (180f / Math.PI.toFloat()) /
+                cos(location.latitude * Math.PI.toFloat()/180f)
+        return Location(longitude, latitude)
+    }
+    private fun jiggleCenter(jiggleRate: Float): Location {
+        val random = Random()
+        val dx = jiggleRate * radiusInMeters * random.nextFloat()
+        val dy = jiggleRate * radiusInMeters * random.nextFloat()
+        return addXYToLocation(center, dx, dy)
+    }
+    fun toGeoPolygon(pointsPerCircle: Int = 16, jiggleRate: Float = 0f): Polygon{
         if (pointsPerCircle < 3){
             throw IllegalArgumentException()
         }
+        val jiggledCenter = jiggleCenter(jiggleRate)
+        val jiggleRadiusMeters = jiggleRate * radiusInMeters
+        val random = Random()
         return Polygon(
             List(pointsPerCircle + 1) { i ->
+                val jiggledRadius = jiggleRadiusMeters * random.nextFloat()
                 val angle = ((i % pointsPerCircle) * 2 * Math.PI / pointsPerCircle).toFloat()
-                val dx = radiusInMeters * cos(angle)
-                val dy = radiusInMeters * sin(angle)
-                val latitude  = center.latitude  + (dy / earthRadiusInMeters) * (180f / Math.PI.toFloat())
-                val longitude = center.longitude + (dx / earthRadiusInMeters) * (180f / Math.PI.toFloat()) /
-                        cos(center.latitude * Math.PI.toFloat()/180f)
-                Location(longitude, latitude)
+                val dx = jiggledRadius * cos(angle)
+                val dy = jiggledRadius * sin(angle)
+                addXYToLocation(jiggledCenter, dx, dy)
             }
         )
     }
