db3eae068f403483f761ab21f5ee6b96f393a834
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-08-04 19:33:33 +0300

    remove cache

--- src/main/we/rashchenko/patronum/database/mongo/MongoUsersDatabase.kt
+++ src/main/we/rashchenko/patronum/database/mongo/MongoUsersDatabase.kt
@@ -10,32 +10,20 @@ import we.rashchenko.patronum.database.stats.GlobalStats
 class MongoUsersDatabase(
     private val usersCollection: MongoCollection<PatronUser>
 ) : UsersDatabase {
-    private val cache = mutableMapOf<String, PatronUser>()
-    private val telegramCache = mutableMapOf<Long, PatronUser>()
 
     override fun new(user: PatronUser) {
         usersCollection.insertOne(user)
-        cache[user.id] = user
-        telegramCache[user.telegramId] = user
     }
 
     override fun get(id: String): PatronUser? {
-        return cache[id] ?: usersCollection.find(Filters.eq("_id", ObjectId(id))).firstOrNull()?.also {
-            cache[id] = it
-            telegramCache[it.telegramId] = it
-        }
+        return usersCollection.find(Filters.eq("_id", ObjectId(id))).firstOrNull()
     }
 
     override fun getByTelegramId(telegramId: Long): PatronUser? {
-        return telegramCache[telegramId] ?: usersCollection.find(Filters.eq("telegramId", telegramId)).firstOrNull()?.also {
-            cache[it.id] = it
-            telegramCache[it.telegramId] = it
-        }
+        return usersCollection.find(Filters.eq("telegramId", telegramId)).firstOrNull()
     }
 
     override fun update(user: PatronUser) {
-        cache[user.id] = user
-        telegramCache[user.telegramId] = user
         usersCollection.replaceOne(Filters.eq("_id", ObjectId(user.id)), user)
     }
 
