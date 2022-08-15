9d6f81245ac05d0312ac01dc995d8cb78dbfe3fa
Dmitrii Rashchenko <d.rashchenko@planner5d.com>
2022-08-15 21:27:28 +0300

    set up for linux

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.taskdefs.condition.Os.*
+
 plugins {
 	id("maven-publish")
 	kotlin("jvm") version "1.7.10"
@@ -7,7 +9,6 @@ repositories {
 	mavenCentral()
 	maven("https://jitpack.io")
 	maven("https://mvn.mchv.eu/repository/mchv/")
-	mavenLocal()
 }
 
 dependencies {
@@ -17,14 +18,23 @@ dependencies {
 	implementation("org.mongodb:mongodb-driver-sync:4.6.0")
 	implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.7")
     implementation(kotlin("script-runtime"))
-}
-
-dependencies {
-	implementation(platform("it.tdlight:tdlight-java-bom:1.0.0.0-SNAPSHOT"))
-	implementation("it.tdlight:tdlight-java:1.0.0.0-SNAPSHOT")
-	implementation("it.tdlight:tdlight-natives-osx-aarch64:4.0.0-SNAPSHOT")
 	implementation("org.slf4j:slf4j-api:1.7.36")
 	implementation("org.slf4j:slf4j-simple:1.7.36")
+}
+
+if (isFamily(FAMILY_MAC) && isArch("aarch64")){
+	dependencies {
+		implementation(platform("it.tdlight:tdlight-java-bom:1.0.0.0-SNAPSHOT"))
+		implementation("it.tdlight:tdlight-java:1.0.0.0-SNAPSHOT")
+		implementation("it.tdlight:tdlight-natives-osx-aarch64:4.0.0-SNAPSHOT")
+	}
+}
+else{
+	dependencies {
+		implementation(platform("it.tdlight:tdlight-java-bom:2.8.4.1"))
+		implementation("it.tdlight:tdlight-java:2.8.4.1")
+		implementation("it.tdlight:tdlight-natives-linux-amd64:1.8.5")
+	}
 }
 
 // we need to specify following sourceSets because we store main and test not in default
